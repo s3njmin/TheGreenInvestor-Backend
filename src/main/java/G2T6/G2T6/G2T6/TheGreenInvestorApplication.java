1e53182c2bb0e0a38c8552bd56c6610ebd8d4870
Chen Kun <chenkun8991@gmail.com>
2022-09-19 10:53:25 +0800

    Spring initializr
    Time to start on the project

---
+++ src/main/java/G2T6/G2T6/G2T6/TheGreenInvestorApplication.java
@@ -0,0 +1,13 @@
+package G2T6.G2T6.G2T6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGreenInvestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGreenInvestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/G2T6/G2T6/G2T6/TheGreenInvestorApplicationTests.java
@@ -0,0 +1,13 @@
+package G2T6.G2T6.G2T6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGreenInvestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
