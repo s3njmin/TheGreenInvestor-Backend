3f368aef3771b2e59f95437c3eb633599e028bf1
benjaminsulh <slh.benjamin@gmail.com>
2022-09-26 10:26:42 +0800

    Option class created

--- src/main/java/G2T6/G2T6/G2T6/options/Option.java
+++ src/main/java/G2T6/G2T6/G2T6/options/Option.java
@@ -1,1 +1,22 @@
+package G2T6.G2T6.G2T6.options;
 
+import G2T6.G2T6.G2T6.questions.Question;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Option {
+    private @Id @GeneratedValue Long id;
+    private String option;
+    
+    @ManyToOne
+    @JoinColumn(name="question_id", nullable=false)
+
+    private Question question;
+}
