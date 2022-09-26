891f2bed1ee9d6f27feca5af51a36b25580c9847
benjaminsulh <slh.benjamin@gmail.com>
2022-09-26 10:43:27 +0800

    GT-40 Question Service Created

---
+++ src/main/java/G2T6/G2T6/G2T6/questions/QuestionService.java
@@ -0,0 +1,11 @@
+package G2T6.G2T6.G2T6.questions;
+
+import java.util.List;
+
+public interface QuestionService {
+    List<Question> listQuestions();
+    Question getQuestion(Long id);
+    Question addQuestion(Question question);
+    Question updaQuestion(Long id, Question question);
+    void deleteQuestion(Long id);
+}
