06b0b3266de98fa210bc247c30f42529740ac9f8
junhua226 <111865920+junhua226@users.noreply.github.com>
2022-09-27 18:41:47 +0800

    added api/** to securityfilterchain
    access gameStats api via
    
    localhost:8080/api/gameStats

--- src/main/java/G2T6/G2T6/G2T6/stats/GameStatsController.java
+++ src/main/java/G2T6/G2T6/G2T6/stats/GameStatsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.*;
 
+@RequestMapping("/api")
 @RestController
 public class GameStatsController {
     private GameStatsService gameStatsService;
