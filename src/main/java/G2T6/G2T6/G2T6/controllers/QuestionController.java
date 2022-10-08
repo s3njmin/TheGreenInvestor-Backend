e267bb2db56294fe29138696b1f3d56dc6b3a8a4
yogesh130 <yogesh@resynctech.com>
2022-10-08 23:34:24 +0800

    edited game

--- src/main/java/G2T6/G2T6/G2T6/controllers/QuestionController.java
+++ src/main/java/G2T6/G2T6/G2T6/controllers/QuestionController.java
@@ -10,17 +10,9 @@ import G2T6.G2T6.G2T6.models.Question;
 import G2T6.G2T6.G2T6.services.QuestionService;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-// @CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api")
 @RestController
 public class QuestionController {
