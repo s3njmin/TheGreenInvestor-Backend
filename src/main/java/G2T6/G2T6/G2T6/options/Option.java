13c9e2d94639407895f4b0ac3a3e9118e3836f3f
benjaminsulh <slh.benjamin@gmail.com>
2022-10-08 15:58:46 +0800

    GT-116 Populate Qn, Options for Demo

--- src/main/java/G2T6/G2T6/G2T6/options/Option.java
+++ src/main/java/G2T6/G2T6/G2T6/options/Option.java
@@ -12,7 +12,8 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Option {
-    private @Id @GeneratedValue Long id;
+    // private @Id @GeneratedValue Long id;
+    private @Id Long id;
     private String option;
     private String feedback;
     
