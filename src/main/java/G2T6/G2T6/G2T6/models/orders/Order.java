9754d71a333b2ae45e97e6cc5ff6a05a00d5b579
benjaminsulh <slh.benjamin@gmail.com>
2022-11-03 15:22:47 +0800

    GT-197 refactored Order Class

---
+++ src/main/java/G2T6/G2T6/G2T6/models/orders/Order.java
@@ -0,0 +1,35 @@
+package G2T6.G2T6.G2T6.models.orders;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public abstract class Order {
+    // id is the primary key of question order in questionOrderRepository
+    private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
+
+    // indexArray stores array of indexes
+    private ArrayList<Integer> indexArray;
+
+    // generates an array containing 1 to (size)
+    public ArrayList<Integer> generateArray(int size) {
+        // generate arraylist
+        ArrayList<Integer> generatedArray = new ArrayList<>();
+        for (int i = 1; i < size; i++) {
+            generatedArray.add(i);
+        }
+
+        return generatedArray;
+    }
+
+    // defined in children classes how to manipulate array
+    public abstract ArrayList<Integer> manipulateArray(ArrayList<Integer> array);
+}
